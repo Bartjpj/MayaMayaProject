c4e32b1189e738b99e3a603fb2dcdc51576a330d
Bartjpj <bartstanjanssen@hotmail.com>
2016-05-13 17:00:52 +0200

    BarOverzicht(keukenveranderd)

--- ProjectMayaMaya/WindowsFormsApplication1/BarOverzicht.Designer.cs
+++ ProjectMayaMaya/WindowsFormsApplication1/BarOverzicht.Designer.cs
@@ -28,11 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbox_BarBestelling = new System.Windows.Forms.GroupBox();
+            this.lbl_OpenstaandeBestellingenKeuken = new System.Windows.Forms.Label();
+            this.btn_BARnaarHM = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gbox_BarBestelling
+            // 
+            this.gbox_BarBestelling.Location = new System.Drawing.Point(12, 53);
+            this.gbox_BarBestelling.Name = "gbox_BarBestelling";
+            this.gbox_BarBestelling.Size = new System.Drawing.Size(211, 145);
+            this.gbox_BarBestelling.TabIndex = 0;
+            this.gbox_BarBestelling.TabStop = false;
+            this.gbox_BarBestelling.Text = "Bestelling X";
+            // 
+            // lbl_OpenstaandeBestellingenKeuken
+            // 
+            this.lbl_OpenstaandeBestellingenKeuken.AutoSize = true;
+            this.lbl_OpenstaandeBestellingenKeuken.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_OpenstaandeBestellingenKeuken.Location = new System.Drawing.Point(33, 26);
+            this.lbl_OpenstaandeBestellingenKeuken.Name = "lbl_OpenstaandeBestellingenKeuken";
+            this.lbl_OpenstaandeBestellingenKeuken.Size = new System.Drawing.Size(237, 24);
+            this.lbl_OpenstaandeBestellingenKeuken.TabIndex = 1;
+            this.lbl_OpenstaandeBestellingenKeuken.Text = "Openstaande Bestellingen:";
+            // 
+            // btn_BARnaarHM
+            // 
+            this.btn_BARnaarHM.Location = new System.Drawing.Point(584, 404);
+            this.btn_BARnaarHM.Name = "btn_BARnaarHM";
+            this.btn_BARnaarHM.Size = new System.Drawing.Size(185, 46);
+            this.btn_BARnaarHM.TabIndex = 2;
+            this.btn_BARnaarHM.Text = "Terug naar Hoofdmenu";
+            this.btn_BARnaarHM.UseVisualStyleBackColor = true;
+            // 
+            // BarOverzicht
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 463);
+            this.Controls.Add(this.btn_BARnaarHM);
+            this.Controls.Add(this.lbl_OpenstaandeBestellingenKeuken);
+            this.Controls.Add(this.gbox_BarBestelling);
+            this.Name = "BarOverzicht";
             this.Text = "BarOverzicht";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbox_BarBestelling;
+        private System.Windows.Forms.Label lbl_OpenstaandeBestellingenKeuken;
+        private System.Windows.Forms.Button btn_BARnaarHM;
     }
 }
--- ProjectMayaMaya/WindowsFormsApplication1/keukenOverzicht.Designer.cs
+++ ProjectMayaMaya/WindowsFormsApplication1/keukenOverzicht.Designer.cs
@@ -29,9 +29,10 @@
         private void InitializeComponent()
         {
             this.groep_BestellingKeukenX = new System.Windows.Forms.GroupBox();
-            this.lbl_BestellingKeukenX = new System.Windows.Forms.Label();
             this.verticaal_ScrollBar = new System.Windows.Forms.VScrollBar();
+            this.lbl_BestellingKeukenX = new System.Windows.Forms.Label();
             this.btn_KeukenNaarHoofdmenu = new System.Windows.Forms.Button();
+            this.lbl_OpenstaandeBestellingenKeuken = new System.Windows.Forms.Label();
             this.groep_BestellingKeukenX.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,50 +40,67 @@
             // 
             this.groep_BestellingKeukenX.Controls.Add(this.verticaal_ScrollBar);
             this.groep_BestellingKeukenX.Controls.Add(this.lbl_BestellingKeukenX);
-            this.groep_BestellingKeukenX.Location = new System.Drawing.Point(12, 12);
+            this.groep_BestellingKeukenX.Location = new System.Drawing.Point(11, 51);
+            this.groep_BestellingKeukenX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groep_BestellingKeukenX.Name = "groep_BestellingKeukenX";
-            this.groep_BestellingKeukenX.Size = new System.Drawing.Size(339, 220);
+            this.groep_BestellingKeukenX.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groep_BestellingKeukenX.Size = new System.Drawing.Size(254, 179);
             this.groep_BestellingKeukenX.TabIndex = 0;
             this.groep_BestellingKeukenX.TabStop = false;
             this.groep_BestellingKeukenX.Text = "Bestelling X";
             // 
+            // verticaal_ScrollBar
+            // 
+            this.verticaal_ScrollBar.Location = new System.Drawing.Point(229, 15);
+            this.verticaal_ScrollBar.Name = "verticaal_ScrollBar";
+            this.verticaal_ScrollBar.Size = new System.Drawing.Size(21, 152);
+            this.verticaal_ScrollBar.TabIndex = 1;
+            // 
             // lbl_BestellingKeukenX
             // 
             this.lbl_BestellingKeukenX.AutoSize = true;
-            this.lbl_BestellingKeukenX.Location = new System.Drawing.Point(7, 22);
+            this.lbl_BestellingKeukenX.Location = new System.Drawing.Point(5, 18);
+            this.lbl_BestellingKeukenX.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_BestellingKeukenX.Name = "lbl_BestellingKeukenX";
-            this.lbl_BestellingKeukenX.Size = new System.Drawing.Size(46, 17);
+            this.lbl_BestellingKeukenX.Size = new System.Drawing.Size(35, 13);
             this.lbl_BestellingKeukenX.TabIndex = 0;
             this.lbl_BestellingKeukenX.Text = "label1";
             // 
-            // verticaal_ScrollBar
-            // 
-            this.verticaal_ScrollBar.Location = new System.Drawing.Point(305, 18);
-            this.verticaal_ScrollBar.Name = "verticaal_ScrollBar";
-            this.verticaal_ScrollBar.Size = new System.Drawing.Size(21, 187);
-            this.verticaal_ScrollBar.TabIndex = 1;
-            // 
             // btn_KeukenNaarHoofdmenu
             // 
-            this.btn_KeukenNaarHoofdmenu.Location = new System.Drawing.Point(823, 456);
+            this.btn_KeukenNaarHoofdmenu.Location = new System.Drawing.Point(617, 370);
+            this.btn_KeukenNaarHoofdmenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_KeukenNaarHoofdmenu.Name = "btn_KeukenNaarHoofdmenu";
-            this.btn_KeukenNaarHoofdmenu.Size = new System.Drawing.Size(178, 34);
+            this.btn_KeukenNaarHoofdmenu.Size = new System.Drawing.Size(134, 28);
             this.btn_KeukenNaarHoofdmenu.TabIndex = 1;
             this.btn_KeukenNaarHoofdmenu.Text = "Terug naar hoofdmenu";
             this.btn_KeukenNaarHoofdmenu.UseVisualStyleBackColor = true;
             // 
+            // lbl_OpenstaandeBestellingenKeuken
+            // 
+            this.lbl_OpenstaandeBestellingenKeuken.AutoSize = true;
+            this.lbl_OpenstaandeBestellingenKeuken.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_OpenstaandeBestellingenKeuken.Location = new System.Drawing.Point(36, 25);
+            this.lbl_OpenstaandeBestellingenKeuken.Name = "lbl_OpenstaandeBestellingenKeuken";
+            this.lbl_OpenstaandeBestellingenKeuken.Size = new System.Drawing.Size(242, 24);
+            this.lbl_OpenstaandeBestellingenKeuken.TabIndex = 2;
+            this.lbl_OpenstaandeBestellingenKeuken.Text = "Openstaande Bestellingen: ";
+            // 
             // keukenBestellingOverzicht
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1013, 502);
+            this.ClientSize = new System.Drawing.Size(760, 408);
+            this.Controls.Add(this.lbl_OpenstaandeBestellingenKeuken);
             this.Controls.Add(this.btn_KeukenNaarHoofdmenu);
             this.Controls.Add(this.groep_BestellingKeukenX);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "keukenBestellingOverzicht";
             this.Text = "keukenBestellingOverzicht";
             this.groep_BestellingKeukenX.ResumeLayout(false);
             this.groep_BestellingKeukenX.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,5 +110,6 @@
         private System.Windows.Forms.VScrollBar verticaal_ScrollBar;
         private System.Windows.Forms.Label lbl_BestellingKeukenX;
         private System.Windows.Forms.Button btn_KeukenNaarHoofdmenu;
+        private System.Windows.Forms.Label lbl_OpenstaandeBestellingenKeuken;
     }
 }
