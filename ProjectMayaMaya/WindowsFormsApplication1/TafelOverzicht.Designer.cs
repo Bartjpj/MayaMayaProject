6a0ba5737d82798977752cedc133658e9cffc6a1
Bartjpj <bartstanjanssen@hotmail.com>
2016-05-19 12:01:48 +0200

    TafelOverzicht

---
+++ ProjectMayaMaya/WindowsFormsApplication1/TafelOverzicht.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class TafelOverzicht
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Tafel1 = new System.Windows.Forms.Button();
+            this.btn_Tafel2 = new System.Windows.Forms.Button();
+            this.btn_Tafel3 = new System.Windows.Forms.Button();
+            this.btn_Tafel4 = new System.Windows.Forms.Button();
+            this.btn_Tafel5 = new System.Windows.Forms.Button();
+            this.btn_Tafel6 = new System.Windows.Forms.Button();
+            this.btn_Tafel7 = new System.Windows.Forms.Button();
+            this.btn_Tafel8 = new System.Windows.Forms.Button();
+            this.btn_Tafel9 = new System.Windows.Forms.Button();
+            this.btn_Tafel10 = new System.Windows.Forms.Button();
+            this.btn_TAFELnaarHM = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Tafel1
+            // 
+            this.btn_Tafel1.Location = new System.Drawing.Point(57, 53);
+            this.btn_Tafel1.Name = "btn_Tafel1";
+            this.btn_Tafel1.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel1.TabIndex = 0;
+            this.btn_Tafel1.Text = "Tafel 1";
+            this.btn_Tafel1.UseVisualStyleBackColor = true;
+            this.btn_Tafel1.Click += new System.EventHandler(this.btn_Tafel1_Click);
+            // 
+            // btn_Tafel2
+            // 
+            this.btn_Tafel2.Location = new System.Drawing.Point(168, 53);
+            this.btn_Tafel2.Name = "btn_Tafel2";
+            this.btn_Tafel2.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel2.TabIndex = 1;
+            this.btn_Tafel2.Text = "Tafel 2";
+            this.btn_Tafel2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel3
+            // 
+            this.btn_Tafel3.Location = new System.Drawing.Point(57, 129);
+            this.btn_Tafel3.Name = "btn_Tafel3";
+            this.btn_Tafel3.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel3.TabIndex = 2;
+            this.btn_Tafel3.Text = "Tafel 3";
+            this.btn_Tafel3.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel4
+            // 
+            this.btn_Tafel4.Location = new System.Drawing.Point(168, 129);
+            this.btn_Tafel4.Name = "btn_Tafel4";
+            this.btn_Tafel4.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel4.TabIndex = 3;
+            this.btn_Tafel4.Text = "Tafel 4";
+            this.btn_Tafel4.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel5
+            // 
+            this.btn_Tafel5.Location = new System.Drawing.Point(57, 205);
+            this.btn_Tafel5.Name = "btn_Tafel5";
+            this.btn_Tafel5.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel5.TabIndex = 4;
+            this.btn_Tafel5.Text = "Tafel 5";
+            this.btn_Tafel5.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel6
+            // 
+            this.btn_Tafel6.Location = new System.Drawing.Point(168, 205);
+            this.btn_Tafel6.Name = "btn_Tafel6";
+            this.btn_Tafel6.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel6.TabIndex = 5;
+            this.btn_Tafel6.Text = "Tafel 6";
+            this.btn_Tafel6.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel7
+            // 
+            this.btn_Tafel7.Location = new System.Drawing.Point(57, 281);
+            this.btn_Tafel7.Name = "btn_Tafel7";
+            this.btn_Tafel7.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel7.TabIndex = 6;
+            this.btn_Tafel7.Text = "Tafel 7";
+            this.btn_Tafel7.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel8
+            // 
+            this.btn_Tafel8.Location = new System.Drawing.Point(168, 281);
+            this.btn_Tafel8.Name = "btn_Tafel8";
+            this.btn_Tafel8.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel8.TabIndex = 7;
+            this.btn_Tafel8.Text = "Tafel 8";
+            this.btn_Tafel8.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel9
+            // 
+            this.btn_Tafel9.Location = new System.Drawing.Point(57, 357);
+            this.btn_Tafel9.Name = "btn_Tafel9";
+            this.btn_Tafel9.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel9.TabIndex = 8;
+            this.btn_Tafel9.Text = "Tafel 9";
+            this.btn_Tafel9.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tafel10
+            // 
+            this.btn_Tafel10.Location = new System.Drawing.Point(168, 357);
+            this.btn_Tafel10.Name = "btn_Tafel10";
+            this.btn_Tafel10.Size = new System.Drawing.Size(105, 70);
+            this.btn_Tafel10.TabIndex = 9;
+            this.btn_Tafel10.Text = "Tafel 10";
+            this.btn_Tafel10.UseVisualStyleBackColor = true;
+            // 
+            // btn_TAFELnaarHM
+            // 
+            this.btn_TAFELnaarHM.Location = new System.Drawing.Point(576, 385);
+            this.btn_TAFELnaarHM.Name = "btn_TAFELnaarHM";
+            this.btn_TAFELnaarHM.Size = new System.Drawing.Size(157, 42);
+            this.btn_TAFELnaarHM.TabIndex = 10;
+            this.btn_TAFELnaarHM.Text = "Terug naar Hoofdmenu";
+            this.btn_TAFELnaarHM.UseVisualStyleBackColor = true;
+            this.btn_TAFELnaarHM.Click += new System.EventHandler(this.btn_TAFELnaarHM_Click);
+            // 
+            // TafelOverzicht
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 502);
+            this.Controls.Add(this.btn_TAFELnaarHM);
+            this.Controls.Add(this.btn_Tafel10);
+            this.Controls.Add(this.btn_Tafel9);
+            this.Controls.Add(this.btn_Tafel8);
+            this.Controls.Add(this.btn_Tafel7);
+            this.Controls.Add(this.btn_Tafel6);
+            this.Controls.Add(this.btn_Tafel5);
+            this.Controls.Add(this.btn_Tafel4);
+            this.Controls.Add(this.btn_Tafel3);
+            this.Controls.Add(this.btn_Tafel2);
+            this.Controls.Add(this.btn_Tafel1);
+            this.Name = "TafelOverzicht";
+            this.Text = "Tafels";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Tafel1;
+        private System.Windows.Forms.Button btn_Tafel2;
+        private System.Windows.Forms.Button btn_Tafel3;
+        private System.Windows.Forms.Button btn_Tafel4;
+        private System.Windows.Forms.Button btn_Tafel5;
+        private System.Windows.Forms.Button btn_Tafel6;
+        private System.Windows.Forms.Button btn_Tafel7;
+        private System.Windows.Forms.Button btn_Tafel8;
+        private System.Windows.Forms.Button btn_Tafel9;
+        private System.Windows.Forms.Button btn_Tafel10;
+        private System.Windows.Forms.Button btn_TAFELnaarHM;
+    }
+}
---
+++ ProjectMayaMaya/WindowsFormsApplication1/TafelOverzicht.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class TafelOverzicht : Form
+    {
+        public TafelOverzicht()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_TAFELnaarHM_Click(object sender, EventArgs e)
+        {
+            HoofdMenu TerugNaarHoofdmenu = new HoofdMenu();
+            TerugNaarHoofdmenu.Show();
+            this.Hide();
+        }
+
+        private void btn_Tafel1_Click(object sender, EventArgs e)
+        {
+            btn_Tafel1.BackColor = Color.Red;
+        }
+    }
+}
