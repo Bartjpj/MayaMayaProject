1fef3fcf29f04f3a37eab3b3d3925207adb5fe13
Bartjpj <bartstanjanssen@hotmail.com>
2016-05-20 19:09:22 +0200

    PersoneelsLid

---
+++ ProjectMayaMaya/WindowsFormsApplication1/PersoneelLid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class PersoneelLid
+    {
+        public string naam;
+        public int personeels_id;
+        
+        private void bestellingsOverzicht()
+        {
+
+        }
+        public void krijgtSalaris()
+        {
+
+        }
+    }
+}
