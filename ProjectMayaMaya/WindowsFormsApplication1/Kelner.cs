eebf5248dcb7ce8ddb6155f348b7a0d038bc8189
Bartjpj <bartstanjanssen@hotmail.com>
2016-05-20 19:14:57 +0200

    Kelner&barpersoneel

---
+++ ProjectMayaMaya/WindowsFormsApplication1/Kelner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class Kelner
+    {
+        private void bestellingOpnemen()
+        {
+
+        }
+        private void bestellingAfronden()
+        {
+
+        }
+        private void rekeningAfdrukken();
+    }
+}
