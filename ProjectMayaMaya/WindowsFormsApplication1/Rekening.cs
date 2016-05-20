42a50ff9b6f40af8c9ea5b5cfc6bfe7b0567a255
a2o4GitHub <564364@student.inholland.nl>
2016-05-20 17:28:45 +0200

    RekeningClassDiagram

---
+++ ProjectMayaMaya/WindowsFormsApplication1/Rekening.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class Rekening
+    {
+        rekeningEnum betaalWijze; // er zijn 3 betaalwijzes pin, contant creditcard
+        
+        public double totaaPrijs; //veld om totaalprijs in te stoppen
+        public int hoeveelheidMenuitem; // veld om de hoeveelheidMenu-items aan te geven
+
+        private void berekenTotaalPrijs(){
+        }
+        private void geefFooi(){
+
+        }
+        public void printRekening(){}
+        public void betaalWijze(rekeningEnum betaalWijze){}
+        
+
+      
+        }
+    }
+
