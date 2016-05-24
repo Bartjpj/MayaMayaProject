2074c336c75dccf04d073f013e39c323c1c9b7cb
a2o4GitHub <564364@student.inholland.nl>
2016-05-24 21:29:03 +0200

    databaseconnectie

---
+++ ProjectMayaMaya/WindowsFormsApplication1/DatabaseConnectie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    class DatabaseConnectie
+    {
+        public void testSQL()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["BestellingConnectionStringSQL"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString); // connectieobject wordt aangemaakt
+
+            conn.Open();   // connectie wordt opengezet 
+
+            SqlCommand command = new SqlCommand();
+            SqlDataReader reader = command.ExecuteReader(); // datareaderobject
+
+            while (reader.Read())
+            {
+
+            }
+
+        }
+       
+    }
+}
