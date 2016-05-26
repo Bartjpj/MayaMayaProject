2080ae74cedca655572adc7b89ab9dc980527036
Bartjpj <bartstanjanssen@hotmail.com>
2016-05-26 16:26:35 +0200

    Dinerkaaasgdhf

---
+++ ProjectMayaMaya/WindowsFormsApplication1/DrankenKaart.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class DrankenKaart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_diner = new System.Windows.Forms.Label();
+            this.btn_DINERnaarDRANKEN = new System.Windows.Forms.Button();
+            this.btn_addg_id21 = new System.Windows.Forms.Button();
+            this.btn_addg_id20 = new System.Windows.Forms.Button();
+            this.btn_addg_id19 = new System.Windows.Forms.Button();
+            this.lbl_nagerechten = new System.Windows.Forms.Label();
+            this.btn_addg_id18 = new System.Windows.Forms.Button();
+            this.btn_addg_id17 = new System.Windows.Forms.Button();
+            this.btn_addg_id16 = new System.Windows.Forms.Button();
+            this.lbl_Hoofdgerechten = new System.Windows.Forms.Label();
+            this.btn_addg_id13 = new System.Windows.Forms.Button();
+            this.btn_addg_id12 = new System.Windows.Forms.Button();
+            this.btn_addg_id11 = new System.Windows.Forms.Button();
+            this.lbl_Voorgerechten = new System.Windows.Forms.Label();
+            this.lbl_Tussengerechten = new System.Windows.Forms.Label();
+            this.btn_addg_id14 = new System.Windows.Forms.Button();
+            this.btn_addg_id15 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_diner
+            // 
+            this.lbl_diner.AutoSize = true;
+            this.lbl_diner.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_diner.Location = new System.Drawing.Point(124, 39);
+            this.lbl_diner.Name = "lbl_diner";
+            this.lbl_diner.Size = new System.Drawing.Size(63, 25);
+            this.lbl_diner.TabIndex = 30;
+            this.lbl_diner.Text = "Diner";
+            // 
+            // btn_DINERnaarDRANKEN
+            // 
+            this.btn_DINERnaarDRANKEN.BackColor = System.Drawing.Color.Maroon;
+            this.btn_DINERnaarDRANKEN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DINERnaarDRANKEN.ForeColor = System.Drawing.Color.White;
+            this.btn_DINERnaarDRANKEN.Location = new System.Drawing.Point(12, 83);
+            this.btn_DINERnaarDRANKEN.Name = "btn_DINERnaarDRANKEN";
+            this.btn_DINERnaarDRANKEN.Size = new System.Drawing.Size(300, 52);
+            this.btn_DINERnaarDRANKEN.TabIndex = 29;
+            this.btn_DINERnaarDRANKEN.Text = "Dranken";
+            this.btn_DINERnaarDRANKEN.UseVisualStyleBackColor = false;
+            // 
+            // btn_addg_id21
+            // 
+            this.btn_addg_id21.Location = new System.Drawing.Point(216, 357);
+            this.btn_addg_id21.Name = "btn_addg_id21";
+            this.btn_addg_id21.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id21.TabIndex = 28;
+            this.btn_addg_id21.Text = "Port e Fromage";
+            this.btn_addg_id21.UseVisualStyleBackColor = true;
+            // 
+            // btn_addg_id20
+            // 
+            this.btn_addg_id20.Location = new System.Drawing.Point(114, 357);
+            this.btn_addg_id20.Name = "btn_addg_id20";
+            this.btn_addg_id20.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id20.TabIndex = 27;
+            this.btn_addg_id20.Text = "Cherry Baby";
+            this.btn_addg_id20.UseVisualStyleBackColor = true;
+            // 
+            // btn_addg_id19
+            // 
+            this.btn_addg_id19.Location = new System.Drawing.Point(12, 357);
+            this.btn_addg_id19.Name = "btn_addg_id19";
+            this.btn_addg_id19.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id19.TabIndex = 26;
+            this.btn_addg_id19.Text = "Café Suprise";
+            this.btn_addg_id19.UseVisualStyleBackColor = true;
+            // 
+            // lbl_nagerechten
+            // 
+            this.lbl_nagerechten.AutoSize = true;
+            this.lbl_nagerechten.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nagerechten.Location = new System.Drawing.Point(13, 334);
+            this.lbl_nagerechten.Name = "lbl_nagerechten";
+            this.lbl_nagerechten.Size = new System.Drawing.Size(101, 20);
+            this.lbl_nagerechten.TabIndex = 25;
+            this.lbl_nagerechten.Text = "Nagerechten";
+            // 
+            // btn_addg_id18
+            // 
+            this.btn_addg_id18.Location = new System.Drawing.Point(216, 294);
+            this.btn_addg_id18.Name = "btn_addg_id18";
+            this.btn_addg_id18.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id18.TabIndex = 24;
+            this.btn_addg_id18.Text = "Hertenbiefstuk";
+            this.btn_addg_id18.UseVisualStyleBackColor = true;
+            // 
+            // btn_addg_id17
+            // 
+            this.btn_addg_id17.Location = new System.Drawing.Point(114, 294);
+            this.btn_addg_id17.Name = "btn_addg_id17";
+            this.btn_addg_id17.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id17.TabIndex = 23;
+            this.btn_addg_id17.Text = "Ossenhaas";
+            this.btn_addg_id17.UseVisualStyleBackColor = true;
+            // 
+            // btn_addg_id16
+            // 
+            this.btn_addg_id16.Location = new System.Drawing.Point(12, 294);
+            this.btn_addg_id16.Name = "btn_addg_id16";
+            this.btn_addg_id16.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id16.TabIndex = 22;
+            this.btn_addg_id16.Text = "Kabbeljauw";
+            this.btn_addg_id16.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Hoofdgerechten
+            // 
+            this.lbl_Hoofdgerechten.AutoSize = true;
+            this.lbl_Hoofdgerechten.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Hoofdgerechten.Location = new System.Drawing.Point(13, 271);
+            this.lbl_Hoofdgerechten.Name = "lbl_Hoofdgerechten";
+            this.lbl_Hoofdgerechten.Size = new System.Drawing.Size(125, 20);
+            this.lbl_Hoofdgerechten.TabIndex = 21;
+            this.lbl_Hoofdgerechten.Text = "Hoofdgerechten";
+            // 
+            // btn_addg_id13
+            // 
+            this.btn_addg_id13.Location = new System.Drawing.Point(216, 161);
+            this.btn_addg_id13.Name = "btn_addg_id13";
+            this.btn_addg_id13.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id13.TabIndex = 20;
+            this.btn_addg_id13.Text = "Krab-Zalm koekjes";
+            this.btn_addg_id13.UseVisualStyleBackColor = true;
+            // 
+            // btn_addg_id12
+            // 
+            this.btn_addg_id12.Location = new System.Drawing.Point(114, 161);
+            this.btn_addg_id12.Name = "btn_addg_id12";
+            this.btn_addg_id12.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id12.TabIndex = 19;
+            this.btn_addg_id12.Text = "Paté v. Fazant";
+            this.btn_addg_id12.UseVisualStyleBackColor = true;
+            // 
+            // btn_addg_id11
+            // 
+            this.btn_addg_id11.Location = new System.Drawing.Point(12, 161);
+            this.btn_addg_id11.Name = "btn_addg_id11";
+            this.btn_addg_id11.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id11.TabIndex = 18;
+            this.btn_addg_id11.Text = "Kalfs Tartaar";
+            this.btn_addg_id11.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Voorgerechten
+            // 
+            this.lbl_Voorgerechten.AutoSize = true;
+            this.lbl_Voorgerechten.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Voorgerechten.Location = new System.Drawing.Point(13, 138);
+            this.lbl_Voorgerechten.Name = "lbl_Voorgerechten";
+            this.lbl_Voorgerechten.Size = new System.Drawing.Size(115, 20);
+            this.lbl_Voorgerechten.TabIndex = 17;
+            this.lbl_Voorgerechten.Text = "Voorgerechten";
+            // 
+            // lbl_Tussengerechten
+            // 
+            this.lbl_Tussengerechten.AutoSize = true;
+            this.lbl_Tussengerechten.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Tussengerechten.Location = new System.Drawing.Point(13, 201);
+            this.lbl_Tussengerechten.Name = "lbl_Tussengerechten";
+            this.lbl_Tussengerechten.Size = new System.Drawing.Size(133, 20);
+            this.lbl_Tussengerechten.TabIndex = 31;
+            this.lbl_Tussengerechten.Text = "Tussengerechten";
+            // 
+            // btn_addg_id14
+            // 
+            this.btn_addg_id14.Location = new System.Drawing.Point(12, 224);
+            this.btn_addg_id14.Name = "btn_addg_id14";
+            this.btn_addg_id14.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id14.TabIndex = 32;
+            this.btn_addg_id14.Text = "Provenciale vissoep";
+            this.btn_addg_id14.UseVisualStyleBackColor = true;
+            // 
+            // btn_addg_id15
+            // 
+            this.btn_addg_id15.Location = new System.Drawing.Point(114, 224);
+            this.btn_addg_id15.Name = "btn_addg_id15";
+            this.btn_addg_id15.Size = new System.Drawing.Size(96, 37);
+            this.btn_addg_id15.TabIndex = 33;
+            this.btn_addg_id15.Text = "Consommé v. Fazant";
+            this.btn_addg_id15.UseVisualStyleBackColor = true;
+            // 
+            // DrankenKaart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 427);
+            this.Controls.Add(this.btn_addg_id15);
+            this.Controls.Add(this.btn_addg_id14);
+            this.Controls.Add(this.lbl_Tussengerechten);
+            this.Controls.Add(this.lbl_diner);
+            this.Controls.Add(this.btn_DINERnaarDRANKEN);
+            this.Controls.Add(this.btn_addg_id21);
+            this.Controls.Add(this.btn_addg_id20);
+            this.Controls.Add(this.btn_addg_id19);
+            this.Controls.Add(this.lbl_nagerechten);
+            this.Controls.Add(this.btn_addg_id18);
+            this.Controls.Add(this.btn_addg_id17);
+            this.Controls.Add(this.btn_addg_id16);
+            this.Controls.Add(this.lbl_Hoofdgerechten);
+            this.Controls.Add(this.btn_addg_id13);
+            this.Controls.Add(this.btn_addg_id12);
+            this.Controls.Add(this.btn_addg_id11);
+            this.Controls.Add(this.lbl_Voorgerechten);
+            this.Name = "DrankenKaart";
+            this.Text = "DrankenKaart";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_diner;
+        private System.Windows.Forms.Button btn_DINERnaarDRANKEN;
+        private System.Windows.Forms.Button btn_addg_id21;
+        private System.Windows.Forms.Button btn_addg_id20;
+        private System.Windows.Forms.Button btn_addg_id19;
+        private System.Windows.Forms.Label lbl_nagerechten;
+        private System.Windows.Forms.Button btn_addg_id18;
+        private System.Windows.Forms.Button btn_addg_id17;
+        private System.Windows.Forms.Button btn_addg_id16;
+        private System.Windows.Forms.Label lbl_Hoofdgerechten;
+        private System.Windows.Forms.Button btn_addg_id13;
+        private System.Windows.Forms.Button btn_addg_id12;
+        private System.Windows.Forms.Button btn_addg_id11;
+        private System.Windows.Forms.Label lbl_Voorgerechten;
+        private System.Windows.Forms.Label lbl_Tussengerechten;
+        private System.Windows.Forms.Button btn_addg_id14;
+        private System.Windows.Forms.Button btn_addg_id15;
+
+    }
+}
