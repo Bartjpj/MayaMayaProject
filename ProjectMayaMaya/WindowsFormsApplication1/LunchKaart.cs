d2e593005552951eb2a2b8169e2ffd088ba31c23
Bartjpj <bartstanjanssen@hotmail.com>
2016-05-26 14:45:32 +0200

    lunchkaart BEGIN

--- ProjectMayaMaya/WindowsFormsApplication1/LunchKaart.cs
+++ ProjectMayaMaya/WindowsFormsApplication1/LunchKaart.cs
@@ -12,9 +12,20 @@ namespace WindowsFormsApplication1
 {
     public partial class LunchKaart : Form
     {
+        protected override void OnLoad(EventArgs e) // is de verwijzing, niets veranderen AUB
+        {
+            base.OnLoad(e);
+            this.Location = Owner.Location;
+            this.Size = Owner.Size;
+        }
         public LunchKaart()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
