99f2aeffa07cb41adc4aaf845262d784c61fda22
ivotjebot <game4life49@live.nl>
2016-05-30 16:58:09 +0200

    Alle DAO's zijn af

--- ProjectMayaMaya/WindowsFormsApplication1/DinerKaartClass.cs
+++ ProjectMayaMaya/WindowsFormsApplication1/DinerKaartClass.cs
@@ -9,6 +9,22 @@ namespace WindowsFormsApplication1
     
     class DinerKaartClass
     {
+        //Variabelen worden gedefinieerd
+        public int kaart_id { get; set; }
+        public int categorie_id { get; set; }
+        public string naam { get; set; }
+        public int prijs { get; set; }
+        public int voorraad { get; set; }
 
+        //De constructor wordt aangemaakt
+        public DinerKaartClass(int kaart_id, int categorie_id, string naam, int prijs, int voorraad)
+        {
+            this.kaart_id = kaart_id;
+            this.categorie_id = categorie_id;
+            this.naam = naam;
+            this.prijs = prijs;
+            this.voorraad = voorraad;
+        }
     }
+    
 }
