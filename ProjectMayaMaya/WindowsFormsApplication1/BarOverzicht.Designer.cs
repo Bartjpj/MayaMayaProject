372324ccad931616fc218cbdc9016a513499ced3
a2o4GitHub <564364@student.inholland.nl>
2016-06-08 20:58:04 +0200

    nieuwste ravi shit

--- ProjectMayaMaya/WindowsFormsApplication1/BarOverzicht.Designer.cs
+++ ProjectMayaMaya/WindowsFormsApplication1/BarOverzicht.Designer.cs
@@ -40,6 +40,7 @@
             this.Item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btn_refresh = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_OpenstaandeBestellingenKeuken
@@ -136,11 +137,21 @@
             this.flowLayoutPanel1.TabIndex = 11;
             this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 45);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // BarOverzicht
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(790, 463);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.btn_refresh);
             this.Controls.Add(this.listView1);
@@ -149,7 +160,6 @@
             this.Controls.Add(this.lbl_OpenstaandeBestellingenKeuken);
             this.Name = "BarOverzicht";
             this.Text = "BarOverzicht";
-      //      this.Load += new System.EventHandler(this.BarOverzicht_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,5 +179,6 @@
         private System.Windows.Forms.ColumnHeader Item;
         private System.Windows.Forms.Button btn_refresh;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
     }
 }
