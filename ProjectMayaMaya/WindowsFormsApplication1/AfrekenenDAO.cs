e4688864bbc58d7367be48c848c52359336f4277
ivotjebot <game4life49@live.nl>
2016-06-16 20:01:42 +0200

    kloteErrors

--- ProjectMayaMaya/WindowsFormsApplication1/AfrekenenDAO.cs
+++ ProjectMayaMaya/WindowsFormsApplication1/AfrekenenDAO.cs
@@ -18,7 +18,7 @@ namespace WindowsFormsApplication1
             SqlConnection conn = new SqlConnection(connString);
             conn.Open();
 
-            SqlCommand command = new SqlCommand("SELECT tafel_id, Aantal,  Menuitem.naam, Menuitem.prijs FROM Bestelling, BestellingItems, Menuitem, Menucategorie, Menukaart WHERE  bestelling_id = BestellingId AND tafel_id = " + tafelNr + " AND ItemId = menu_id AND Menuitem.categorie_id = Menucategorie.categorie_id AND Menukaart.kaart_id = Menucategorie.kaart_id  AND is_betaald = 0 ", conn); // deze query zorgt ervoor dat we alle data hebben die we bij BestellingMenu nodig hebben 
+            SqlCommand command = new SqlCommand("SELECT tafel_id, Aantal, Bestelling.bestelling_id Menuitem.naam, Menuitem.prijs FROM Bestelling, BestellingItems, Menuitem, Menucategorie, Menukaart WHERE  bestelling_id = BestellingId AND tafel_id = " + tafelNr + " AND ItemId = menu_id AND Menuitem.categorie_id = Menucategorie.categorie_id AND Menukaart.kaart_id = Menucategorie.kaart_id  AND is_betaald = 0 ", conn); // deze query zorgt ervoor dat we alle data hebben die we bij BestellingMenu nodig hebben 
             SqlDataReader reader = command.ExecuteReader();
 
             List<AfrekenenBLL> AfrekenTable = new List<AfrekenenBLL>();
@@ -27,6 +27,7 @@ namespace WindowsFormsApplication1
             {
 
                 int tafel_id = (int)reader["tafel_id"];
+                // int bestellingID = (int)reader["bestelling_id"];
                 int Aantal = (int)reader["Aantal"];
                 string Menuitem = (string)reader["naam"];
                 double prijs = (double)reader["prijs"];
@@ -38,6 +39,28 @@ namespace WindowsFormsApplication1
 
             conn.Close();
             return AfrekenTable;
+        }
+
+        public List<int> haalRekeningIdOp()
+        {
+            string connString = ConfigurationManager
+            .ConnectionStrings["BestellingConnectionStringSQL"]
+            .ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+
+            SqlCommand command = new SqlCommand("SELECT rekening_id FROM Rekening", conn); // deze query zorgt ervoor dat we alle data hebben die we bij BestellingMenu nodig hebben 
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<int> rekeningIDs = new List<int>();
+
+            while (reader.Read())
+            {
+                int rekening_id = (int)reader["rekening_id"];
+                rekeningIDs.Add(rekening_id);
+            }
+            conn.Close();
+            return rekeningIDs;
         }
     }
 }
