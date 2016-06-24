3a52fbf066ff502ddabd48d9f36ae2b0187911b0
a2o4GitHub <564364@student.inholland.nl>
2016-06-24 16:07:44 +0200

    ff

--- ProjectMayaMaya/WindowsFormsApplication1/BarOverzicht.cs
+++ ProjectMayaMaya/WindowsFormsApplication1/BarOverzicht.cs
@@ -86,7 +86,7 @@ namespace WindowsFormsApplication1
             else { opmerking = " "; }
             lijstItem.SubItems.Add(opmerking);
                 listView1.Items.Add(lijstItem);
-        } // methode om listviewItems toe te voegen cc
+        } // methode om listviewItems toe te voegen 
        
         private void DisplayBestellingen()
         {
@@ -116,7 +116,7 @@ namespace WindowsFormsApplication1
 
             for (int i = listView1.Items.Count - 1; i >= 0; i--) // selecteer eerste item van listview indien geklikt op button in flowpanel om vervolgens te verwijderen
             {
-                if (listView1.Items[i].Selected)// als listview item selected dan fverwijderen op moment dat er geklikt wordt.
+                if (listView1.Items[i].Selected)// als listview item selected dan verwijderen op moment dat er geklikt wordt.
                 {
                     listView1.Items[i].Remove();
 
